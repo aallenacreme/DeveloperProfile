73acc77d6c974e074908811225963547fe7a7504
allenmahdi <dragonking272@gmail.com>
2025-06-16 13:09:45 -0500

    new supa rainbow

--- src/app/components/axiosInstance.jsx
+++ src/app/components/axiosInstance.jsx
@@ -1,28 +1,27 @@
-// components/axiosInstance.js
-import axios from 'axios';
+// // components/axiosInstance.js
+// import axios from 'axios';
 
-// Public instance (no forced authentication)
-const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+// // Public instance (no forced authentication)
+// const axiosInstance = axios.create({
+//   baseURL: 'http://localhost:5000/api',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
 
+// const axiosAuthInstance = axios.create({
+//   baseURL: 'http://localhost:5000/api',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
 
-const axiosAuthInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+// axiosAuthInstance.interceptors.request.use((config) => {
+//   const token = localStorage.getItem('token');
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
 
-axiosAuthInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-export { axiosInstance, axiosAuthInstance }; // Named exports
+// export { axiosInstance, axiosAuthInstance }; // Named exports
